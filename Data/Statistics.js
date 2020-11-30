84fce2e741ed9abbbaa2e5992485bc62cb820c40
Aleksander Kondrat <it.kondrat@gmail.com>
2020-11-30 15:01:45 +0100

    002 Add Statistic List View

---
+++ Data/Statistics.js
@@ -0,0 +1,59 @@
+
+export const statistics = [
+    {
+        key: "attack",
+        name: "Atak",
+        image: require('../Images/Interface/att.jpg'),
+    },
+    {
+        key: "deffence",
+        name: "Obrona",
+        image: require('../Images/Interface/def.jpg'),
+    },
+    {
+        key: "shots",
+        name: "Strzały",
+        image: require('../Images/Interface/shots.jpg'),
+    },
+    {
+        key: "dmg",
+        name: "Obrażenia",
+        image: require('../Images/Interface/dmg.jpg'),
+    },
+    {
+        key: "hp",
+        name: "Wytrzymałość",
+        image: require('../Images/Interface/hp.jpg'),
+    },
+    {
+        key: "speed",
+        name: "Szybkość",
+        image: require('../Images/Interface/speed.jpg'),
+    },
+    {
+        key: "cost",
+        name: "Koszt",
+        image: require('../Images/Interface/cost.jpg'),
+    },
+    {
+        key: "growth",
+        name: "Przyrost",
+        image: require('../Images/Interface/growth.jpg'),
+    },
+    {
+        key: "heroesWithSpeciality",
+        name: "Bohater",
+        image: require('../Images/Interface/heroes.jpg'),
+    },
+    {
+        key: "aiValue",
+        name: "Wartość AI",
+        image: require('../Images/Interface/emptySmall.jpg'),
+    },
+    {
+        key: "lvl",
+        name: "Poziom",
+        image: require('../Images/Interface/emptySmall.jpg'),
+    },
+
+];
